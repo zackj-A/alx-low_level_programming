b3a97d7b32ab6176942d1e1d30b4aff64264dee7
zackj-A <zacheusjohnakurugu@gmail.com>
2023-07-14 07:57:36 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is to create and output strings formatted at runtime
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
