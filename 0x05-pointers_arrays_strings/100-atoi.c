ceb9bf072c2e22b819cdf2b6eefb189c7212f849
zackj-A <zacheusjohnakurugu@gmail.com>
2023-07-26 09:18:25 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,20 +11,14 @@ int _atoi(char *s)
 	int i = 0;
 
 	if (s[0] == '-')
-	{
 		sign = -1;
-		i--;
-	}
+		i++;
 	for (; s[i] != '\0'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
-		{
 			result = (result * 10) + (s[i] - '0');
-		}
 		else if (result != 0)
-		{
 			break;
-		}
 	}
 	return (result * sign);
 }
