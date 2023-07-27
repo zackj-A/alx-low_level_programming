8babb38337037dd8029a0b72c73baa2fc88ca6c7
zackj-A <zacheusjohnakurugu@gmail.com>
2023-07-27 07:47:34 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: dest string
+ * @src: source string
+ * @n: number of bytes to be counted
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest);
+	int a;
+
+	for (a = 0 ; a < n && *src != '\0' ; a++)
+	{
+		dest[len + a] = src[a];
+		src++;
+	}
+	dest[len + a] = '\0';
+	return (dest);
+}
